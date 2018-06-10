0a1983eda65333ba141c628a1249aa6706353b56
Andrew Peterson <cybernetico@gmail.com>
2018-06-10 18:43:52 -0400

    Blog integration.

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -42,7 +42,15 @@ const styles = theme => ({
         marginBottom: theme.spacing.unit * 2
     },
     paper: {
-        padding: theme.spacing.unit * 2,
+        [theme.breakpoints.up('sm')]: {
+            padding: theme.spacing.unit * 2
+        },
+        [theme.breakpoints.down('xs')]: {
+            paddingTop: theme.spacing.unit * 2,
+            paddingBottom: theme.spacing.unit * 2,
+            paddingLeft: theme.spacing.unit,
+            paddingRight: theme.spacing.unit
+        },
         textAlign: 'left',
         color: theme.palette.text.secondary,
     },
@@ -55,19 +63,151 @@ const styles = theme => ({
     },
     messageBoxContainer: {
         margin: -spacing
+    },
+    featuredImage: {
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "50% 50%",
+        backgroundSize: "cover",
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: -theme.spacing.unit * 2,
+            marginRight: -theme.spacing.unit * 2,
+            height: '240px'
+        },
+        [theme.breakpoints.down('xs')]: {
+            marginLeft: -theme.spacing.unit,
+            marginRight: -theme.spacing.unit,
+            height: '180px'
+        },
+        textShadow: "4px 4px 4px #000000",
+        paddingBottom: theme.spacing.unit * 2
+    },
+    featuredHeadline: {
+        display: "table-cell",
+        verticalAlign: "bottom",
+        [theme.breakpoints.up('sm')]: {
+            paddingLeft: theme.spacing.unit * 2,
+            height: '240px',
+        },
+        [theme.breakpoints.down('xs')]: {
+            paddingLeft: theme.spacing.unit,
+            height: '180px'
+        },
+    },
+    featuredExcerpt: {
+        marginTop: theme.spacing.unit * 2
     }
 });
 
 let blogRequested = false
-let blogContent = <div/>
-let featuredBlogContent = <div/>
 let blogResponse = null
 let blogFeaturedResponse = null
+let allPosts = null
+let featuredPost = null
+
+const featuredBlogSection = props => {
+    if(featuredPost != null) {
+        return (
+            <Grid item xs={12}>
+                <Paper className={props.classes.paper}>
+                    <Typography variant="subheading" component="h3" gutterBottom>
+                        Featured
+                    </Typography>
+                    <div>
+                        <div className={props.classes.featuredImage} style={{backgroundImage: 'url(' + "https://blog.head.click" + featuredPost.feature_image + ")"}}>
+                            <div className={props.classes.featuredHeadline}>
+                                <a href={"https://blog.head.click/"+ featuredPost.slug} target="_blank">
+                                    <Typography variant="headline">{featuredPost.title}</Typography>
+                                </a>
+                            </div>
+                        </div>
+                        <div className={props.classes.featuredExcerpt}>
+                            <Typography variant="body2">{featuredPost.custom_excerpt} <a href={"https://blog.head.click/"+ featuredPost.slug} target="_blank">Read More...</a></Typography>
+                        </div>
+                    </div>
+                </Paper>
+            </Grid>
+        )
+    }
+    else if(featuredPost == "error")
+    {
+        return (
+            <Grid item xs={12}>
+                <Paper className={props.classes.paper}>
+                    <Typography variant="subheading" component="h3" gutterBottom>
+                        Featured
+                    </Typography>
+                    <Typography variant="body2">Error loading from blog</Typography>
+                </Paper>
+            </Grid>
+        )
+    }
+    else
+    {
+        return (
+            <Grid item xs={12}>
+                <Paper className={props.classes.paper}>
+                    <Typography variant="subheading" component="h3" gutterBottom>
+                        Featured
+                    </Typography>
+                    <Typography variant="body2">Loading...</Typography>
+                </Paper>
+            </Grid>
+        )
+    }
+}
+
+const blogSection = props => {
+    if(allPosts != null && allPosts != "error") {
+        return (
+            <Grid item xs={12}>
+                <Paper className={props.classes.paper}>
+                    <Typography variant="subheading" component="h3" gutterBottom>
+                        Latest Blog Posts
+                    </Typography>
+                    <List subheader={<li />}>
+                        {allPosts.map(post => (
+                            <ListItem key={post.id}>
+                                <ListItemText
+                                primary={post.title}
+                                secondary={<span>{post.custom_excerpt} <a href={"https://blog.head.click/"+ post.slug} target="_blank">Read More...</a></span>} 
+                                />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Paper>
+            </Grid>
+        )
+    }
+    else if (allPosts == "error") {
+        return (
+            <Grid item xs={12}>
+                <Paper className={props.classes.paper}>
+                    <Typography variant="subheading" component="h3" gutterBottom>
+                        Latest Blog Posts
+                    </Typography>
+                    <Typography variant="body2">Error loading from blog</Typography>
+                </Paper>
+            </Grid>
+        )
+    }
+    else
+    {
+        return (
+            <Grid item xs={12}>
+                <Paper className={props.classes.paper}>
+                    <Typography variant="subheading" component="h3" gutterBottom>
+                        Latest Blog Posts
+                    </Typography>
+                    <Typography variant="body2">Loading...</Typography>
+                </Paper>
+            </Grid>
+        )
+    }
+}
 
 class Dashboard extends React.Component {
     constructor(props){
         super(props)
-        this.state = { blogContent: blogContent, featuredBlogContent: featuredBlogContent }
         // Grab blog content
         if(!blogRequested) {
             // Grab 6 latest blog posts
@@ -78,7 +218,7 @@ class Dashboard extends React.Component {
                     limit: '6',
                     fields: "id, title, slug, featured, custom_excerpt"
                 }})
-                .then(response => { console.log(response); blogResponse = response; this.populateBlogContent() })
+                .then(response => { blogResponse = response; this.populateBlogContent() })
                 .catch(error => console.log(error))
             axios.get('https://blog.head.click/ghost/api/v0.1/posts/', {
                 params: {
@@ -86,9 +226,9 @@ class Dashboard extends React.Component {
                     client_secret: 'f8dee5ed9d9d',
                     limit: '1',
                     filter: "featured:true",
-                    fields: "title, slug, feature_image, featured, custom_excerpt"
+                    fields: "id, title, slug, feature_image, featured, custom_excerpt"
                 }})
-                .then(response => { console.log(response); blogFeaturedResponse = response; this.populateBlogContent() })
+                .then(response => { blogFeaturedResponse = response; this.populateBlogContent() })
                 .catch(error => console.log(error))
             blogRequested = true
         }
@@ -97,51 +237,30 @@ class Dashboard extends React.Component {
     populateBlogContent = () => {
         // Make sure both have returned
         if(blogResponse != null && blogFeaturedResponse != null) {
-            let allPosts = blogResponse.data.posts
-            let featuredPostArray = blogFeaturedResponse
+            allPosts = blogResponse.data.posts
             // See if we got a featured post back at all
-            if(featuredPostArray.lengh > 0)
+            if(blogFeaturedResponse.data.posts.length > 0)
             {
-                // Strip that post out of the other post list
-                allPosts = allPosts.filter()
+                featuredPost = blogFeaturedResponse.data.posts[0]
+                // Strip that post out of the other post list, and cap total at 5
+                allPosts = allPosts.filter(post => post.id != featuredPost.id).slice(0, 4)
             }
-            // Strip the featured response out of all posts, if it exists there
-            
-        }
-
-    }
-
-    populateaBlogContent = response => {
-        blogContent = (
-            <List subheader={<li />}>
-            {response.data.posts.map(post => (
-                <ListItem key={post.id}>
-                    <ListItemText primary={post.title} secondary={<span>{post.custom_excerpt} <a href={"https://blog.head.click/"+ post.slug} target="_blank">Read More...</a></span>} />
-                </ListItem>
-            ))}
-            </List>
-        )
-        this.setState({blogContent: blogContent})
-    }
-
-    populateFeaturedBlogContent = response => {
-        if(response.data.posts.length > 0) {
-            let post = response.data.posts[0]
-            featuredBlogContent = (
+            // Set the rest of the blog content
+            let blogContent = (
                 <List subheader={<li />}>
+                {allPosts.map(post => (
                     <ListItem key={post.id}>
                         <ListItemText primary={post.title} secondary={<span>{post.custom_excerpt} <a href={"https://blog.head.click/"+ post.slug} target="_blank">Read More...</a></span>} />
                     </ListItem>
-                </List>)
+                ))}
+                </List>
+            )
+            // Clear responses from memory
+            blogResponse = null
+            blogFeaturedResponse = null
+            // Update the state
+            this.setState({test: 0})
         }
-        else {
-
-        }
-        this.setState({featuredBlogContent: featuredBlogContent})
-    }
-
-    populateBlogError = error => {
-        
     }
 
     render() {
@@ -197,29 +316,15 @@ class Dashboard extends React.Component {
                     {/* RIGHT COLUMN  */}
                     <Grid item xs={12} xl={6}>
                         <Grid container spacing={spacing}>
+                            {/* Featured Blog */}
+                            {featuredBlogSection(this.props)}
+                            {/* Blog */}
+                            {blogSection(this.props)}
                             {/* development warning card */}
                             <Grid item xs={12}>
                                 <div className={classes.messageBoxContainer}>
                                     <MessageBox>{copy["en"].misc.versionWarning}</MessageBox>
                                 </div>
-                            </Grid>
-                            {/* Featured Blog */}
-                            <Grid item xs={12}>
-                                <Paper className={classes.paper}>
-                                    <Typography variant="subheading" component="h3" gutterBottom>
-                                        Featured
-                                    </Typography>
-                                    {this.state.featuredBlogContent}
-                                </Paper>
-                            </Grid>
-                            {/* Blog */}
-                            <Grid item xs={12}>
-                                <Paper className={classes.paper}>
-                                    <Typography variant="subheading" component="h3" gutterBottom>
-                                        Latest Blog Posts
-                                    </Typography>
-                                    {this.state.blogContent}
-                                </Paper>
                             </Grid>
                         </Grid>
                     </Grid>
